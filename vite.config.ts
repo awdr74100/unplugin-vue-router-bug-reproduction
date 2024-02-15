4a39c8735927c0626b3364cabb105c1da410d070
Yi-Ru Lan <a78945612385238@gmail.com>
2024-02-15 23:11:15 +0800

    Init

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueRouter from 'unplugin-vue-router/vite'
+import electron from 'vite-plugin-electron/simple'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vueRouter({
+      dts: 'src/typed-router.d.ts'
+    }),
+    vue(),
+    electron({
+      main: {
+        entry: 'electron/main.ts'
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
